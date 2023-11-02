11ab7972cb8e1b9368608983b36ea89498ad3f0a
kallyralph <joshuaraphael2016@gmail.com>
2023-11-02 13:57:02 -0700

    simply match file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression
-if ARGV.length == 1
-  input = ARGV[0]
-  if input.match(/School/)
-    puts input
-  end
-end
+puts ARGV[0].scan(/School/).join

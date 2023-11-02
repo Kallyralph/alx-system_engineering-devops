5479ad903da70be18f0803168acfffe40702a54a
kallyralph <joshuaraphael2016@gmail.com>
2023-11-02 12:44:59 -0700

    updated file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+# Check if the argument matches the regular expression
+if ARGV.length == 1
+  input = ARGV[0]
+  if input.match(/School/)
+    puts input
+  end
+end
